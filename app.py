60e6a0e27902552be1b1e01b1592183cb4e61f52
magnusgp <s204075@student.dtu.dk>
2023-11-13 23:29:01 +0100

    updated main app

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def main():
 
     # Plot bar chart with Plotly
     st.subheader("Bar Chart - Chart over Baren")
-    fig = px.bar(sum_by_name, x='name', y='total_value', labels={'total_value': 'Total Value'})
+    fig = px.bar(sum_by_name, x='Navn', y='Beløb', labels={'total_value': 'Total Value'})
     st.plotly_chart(fig)
 
 
